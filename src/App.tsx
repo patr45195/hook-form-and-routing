632409271971aad577b34e56871634830203a460
patr45195 <patr45195@gmail.com>
2024-03-18 21:20:59 +0300

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="layout">
+      <header>Header</header>
+      <div className="content">
+        <div className="sidebar">
+          <div>Page 1</div>
+          <div>Page 2</div>
+          <div>Page 3</div>
+        </div>
+        <main>Main content</main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
