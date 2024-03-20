3d1709028d0fb79b82f7cde51af23d1a820e8a4b
patr45195 <patr45195@gmail.com>
2024-03-20 20:51:21 +0300

    +

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,116 +1,42 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, InputLabel, TextField } from "@mui/material";
-import axios from "axios";
-import { useController, useForm } from "react-hook-form";
+import { SubmitHandler, useController, useForm } from "react-hook-form";
+import { z } from "zod";
 
-interface IFormData {
-  name: string;
-  username: string;
-  email: string;
-  posts: string[];
-}
+const formSchema = z.object({
+  firstName: z.string().min(2).max(50),
+});
 
-interface IUser {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
-
-interface IPost {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
-
-export const fetchDefaultValues = async () => {
-  try {
-    const userPromise = axios.get<IUser>(
-      "https://jsonplaceholder.typicode.com/users/1"
-    );
-    const postsPromise = axios.get<IPost[]>(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-
-    const [userResponse, postsResponse] = await Promise.all([
-      userPromise,
-      postsPromise,
-    ]);
-
-    const user = userResponse.data;
-    const posts = postsResponse.data;
-
-    return {
-      name: user.name,
-      username: user.username,
-      email: user.email,
-      posts: posts.map((post) => post.title),
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      name: "",
-      username: "",
-      email: "",
-      posts: [""],
-    };
-  }
-};
+type FormSchema = z.infer<typeof formSchema>;
 
 export const Form = () => {
-  const { control, handleSubmit } = useForm<IFormData>({
-    defaultValues: async () => await fetchDefaultValues(),
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    defaultValues: {
+      firstName: "Alex",
+    },
+    resolver: zodResolver(formSchema),
   });
 
-  const onSubmit = (data: IFormData) => {
+  const onSubmit: SubmitHandler<FormSchema> = (data) => {
     console.log(data);
   };
 
-  const nameProps = useController({
-    name: "name",
-    control,
-  });
-  const userNameProps = useController({
-    name: "username",
-    control,
-  });
-  const titleProps = useController({
-    name: "email",
-    control,
-  });
-  const postsProps = useController({
-    name: "posts",
+  const firstNameProps = useController({
+    name: "firstName",
     control,
   });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <InputLabel id="name">Name</InputLabel>
+      <InputLabel id="firstName">First Name</InputLabel>
       <TextField
-        {...nameProps.field}
-        value={nameProps.field.value ?? ""}
-        id="name"
-      />
-
-      <InputLabel id="userName">UserName</InputLabel>
-      <TextField
-        {...userNameProps.field}
-        value={userNameProps.field.value ?? ""}
-        id="userName"
-      />
-
-      <InputLabel id="title">Name</InputLabel>
-      <TextField
-        {...titleProps.field}
-        value={titleProps.field.value ?? ""}
-        id="title"
-      />
-
-      <InputLabel id="posts">Posts</InputLabel>
-      <TextField
-        {...postsProps.field}
-        value={postsProps.field.value[0] ?? ""}
-        id="posts"
+        {...firstNameProps.field}
+        value={firstNameProps.field.value ?? ""}
+        id="firstName"
       />
 
       <Button type="submit">Submit</Button>
